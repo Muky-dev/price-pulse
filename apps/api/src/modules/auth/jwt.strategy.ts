b6cecce1d279cd603dd9123d22cdf1bd87186942
Muky <ti.gabrielfini@gmail.com>
2026-06-20 11:48:16 -0300

    fix jwt validator return

--- apps/api/src/modules/auth/jwt.strategy.ts
+++ apps/api/src/modules/auth/jwt.strategy.ts
@@ -14,6 +14,6 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   validate(payload: { sub: string; username: string }) {
-    return { userId: payload.sub, username: payload.username };
+    return { id: payload.sub, username: payload.username };
   }
 }
