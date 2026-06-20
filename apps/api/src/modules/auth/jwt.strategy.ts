4346e95963fb9a93fe49361d3d8485e956676021
Muky <ti.gabrielfini@gmail.com>
2026-06-20 11:48:57 -0300

    type jwt validation return

--- apps/api/src/modules/auth/jwt.strategy.ts
+++ apps/api/src/modules/auth/jwt.strategy.ts
@@ -2,6 +2,7 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { AuthUser } from '../users/entity/user';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -13,7 +14,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     });
   }
 
-  validate(payload: { sub: string; username: string }) {
+  validate(payload: { sub: string; username: string }): AuthUser {
     return { id: payload.sub, username: payload.username };
   }
 }
