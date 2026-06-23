2dc1972aa46b24ded0111188b8421e3500ccf063
Muky <ti.gabrielfini@gmail.com>
2026-06-23 00:45:25 -0300

    update scrape run update DTO

--- apps/api/src/modules/scrape-runs/dto/update-scrape-run.dto.ts
+++ apps/api/src/modules/scrape-runs/dto/update-scrape-run.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateScrapeRunDto } from './create-scrape-run.dto';
 
-export class UpdateScrapeRunDto extends PartialType(CreateScrapeRunDto) {}
+export class UpdateScrapeRunDto extends PartialType(CreateScrapeRunDto) {
+  finishedAt?: Date;
+}
