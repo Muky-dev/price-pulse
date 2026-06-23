63e0276f3e4ddfc3d1d5a693ff039a8bd4c106a8
Muky <ti.gabrielfini@gmail.com>
2026-06-23 00:44:11 -0300

    update registry to include og strategy

--- apps/api/src/modules/scraping/strategy.registry.ts
+++ apps/api/src/modules/scraping/strategy.registry.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { JsonLdStrategy } from './strategies/json-ld.strategy';
 import { MicrodataStrategy } from './strategies/microdata.strategy';
+import { OpenGraphStrategy } from './strategies/open-graph.strategy';
 
 @Injectable()
 export class StrategyRegistry {
   constructor(
     private readonly jsonLdStrategy: JsonLdStrategy,
     private readonly microdataStrategy: MicrodataStrategy,
+    private readonly openGraphStrategy: OpenGraphStrategy,
   ) {}
 
   getAll() {
-    return [this.jsonLdStrategy, this.microdataStrategy];
+    return [
+      this.jsonLdStrategy,
+      this.microdataStrategy,
+      this.openGraphStrategy,
+    ];
   }
 }
