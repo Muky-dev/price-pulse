83ca99fe175ac01e695a9ff8fded5b4c2ce8f1ae
Muky <ti.gabrielfini@gmail.com>
2026-07-01 22:34:04 -0300

    remove scrape run mutations endpoints

--- apps/api/src/modules/scrape-runs/scrape-runs.controller.ts
+++ apps/api/src/modules/scrape-runs/scrape-runs.controller.ts
@@ -1,24 +1,9 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 import { ScrapeRunsService } from './scrape-runs.service';
-import { CreateScrapeRunDto } from './dto/create-scrape-run.dto';
-import { UpdateScrapeRunDto } from './dto/update-scrape-run.dto';
 
 @Controller('scrape-runs')
 export class ScrapeRunsController {
   constructor(private readonly scrapeRunsService: ScrapeRunsService) {}
-
-  @Post()
-  create(@Body() createScrapeRunDto: CreateScrapeRunDto) {
-    return this.scrapeRunsService.create(createScrapeRunDto);
-  }
 
   @Get()
   findAll() {
@@ -28,18 +13,5 @@ export class ScrapeRunsController {
   @Get(':id')
   findOne(@Param('id') id: string) {
     return this.scrapeRunsService.findOne(id);
-  }
-
-  @Patch(':id')
-  update(
-    @Param('id') id: string,
-    @Body() updateScrapeRunDto: UpdateScrapeRunDto,
-  ) {
-    return this.scrapeRunsService.update(id, updateScrapeRunDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.scrapeRunsService.remove(id);
   }
 }
