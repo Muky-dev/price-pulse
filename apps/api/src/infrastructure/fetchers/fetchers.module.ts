ee13a31ba5b6d11aad279827e155123a8cf7e5a0
Muky <ti.gabrielfini@gmail.com>
2026-07-27 23:45:05 -0300

    include http fetcher to fetchers module

--- apps/api/src/infrastructure/fetchers/fetchers.module.ts
+++ apps/api/src/infrastructure/fetchers/fetchers.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { PlaywrightFetcherService } from './playwright-fetcher.service';
 import { BrowserModule } from '../browser/browser.module';
 import { FetchersRegistry } from './fetchers.registry';
+import { HttpFetcherService } from './http-fetcher.service';
 
 @Module({
   imports: [BrowserModule],
-  providers: [PlaywrightFetcherService, FetchersRegistry],
+  providers: [PlaywrightFetcherService, HttpFetcherService, FetchersRegistry],
   exports: [FetchersRegistry],
 })
 export class FetchersModule {}
